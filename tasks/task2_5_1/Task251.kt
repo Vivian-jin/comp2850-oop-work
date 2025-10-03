840089b271d335c062ee24c1545a301aa9fc7d63
Vivian-jin <hnlg9920@leeds.ac.uk>
2025-10-03 11:38:28 +0000

    week1-2

---
+++ tasks/task2_5_1/Task251.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var x=2
+    println(x)
+    x=4
+    println(x)
+}
--- tasks/task4_5/OddSum.kt
+++ tasks/task4_5/OddSum.kt
@@ -1,1 +1,13 @@
 // Task 4.5: summing odd integers with a for loop
+fun main(){
+    print("Please enter your limit: ")
+    val limit=readline()!!.toInt()
+    var sum=0
+    for (i in 1..limit){
+        if i%2 !=0{
+            sum +=i
+        }
+        
+    }
+    println("The sum between 1 and $limit is $sum")
+}
