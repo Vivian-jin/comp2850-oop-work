5bcb3341534d7e8a63d2f4a8fac50dd76b0f2968
Vivian-jin <hnlg9920@leeds.ac.uk>
2025-10-03 15:23:05 +0000

    week1.2

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,12 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size < 2) {
+        println("Error: Need at least 2 arguments")
+        exitProcess(1) 
+    }
+    println(args[0])
+    println(args[1])
+}
+
---
+++ tasks/task3_2/Task32.kt
@@ -0,0 +1,6 @@
+fun main() {
+    print("Enter your name: ")
+    val name = readln()
+    println("Your name has ${name.length} characters")
+
+}
