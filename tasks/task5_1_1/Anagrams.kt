284fa8d3de6d364df59b9922db0038951335b40a
Vivian-jin <hnlg9920@leeds.ac.uk>
2025-10-07 11:54:23 +0000

    week2-1

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,20 @@
 // Task 5.1.1: anagram checking using a function
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
+fun main() {
+    print("Please enter the first string: ")
+    val str1=readln()
+    print("Please enter the secomd strig: ")
+    val str2=readln()
+    if (anagrams(str1,str2)){
+        print("$str1'and '$str2'is anagrams.")
+    } else{
+        print("'$str1' and '$str2' are not anagrams.")
+    }
+}
