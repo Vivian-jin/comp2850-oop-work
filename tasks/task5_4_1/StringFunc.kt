cd30db7bf1b037ffe506a9c3fe7034cfcfd2f932
Vivian-jin <hnlg9920@leeds.ac.uk>
2025-10-14 11:58:08 +0000

    week 3-1

--- tasks/task5_4_1/StringFunc.kt
+++ tasks/task5_4_1/StringFunc.kt
@@ -1,1 +1,11 @@
 // Task 5.4.1: string extension function
+fun String.toLong():Boolean=this.length>20
+fun main(){
+    print("Enter a string:")
+    val input =readln()
+    if (input.toLong()){
+        println("That string is too long!")
+    }else{
+        println("that string is fine.")
+    }
+}
